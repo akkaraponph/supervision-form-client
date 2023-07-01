a995d57cdf4f37935f85303462f30fbbef6aea36
Akkarapon Phikulsri <akkarapon@billowdev.com>
2023-07-01 14:39:40 +0700

    deploy

--- src/app/interfaces/news.ts
+++
@@ -1,8 +0,0 @@
-export interface News {
-    Id_News?: number,
-    Name_News: string,
-    PeopleUpdate_News: string,
-    Content_News: string,
-    Date_News: Date,  
-  
-}
--- src/app/interfaces/qf.interfaces.ts
+++
@@ -1,9 +0,0 @@
-import { APIResponse } from "./api.interfaces"
-
-export interface QFPayload {
-	id: string
-	question: string
-	supervisionFormId?: string
-}
-
-export type QFResponse = APIResponse<QFPayload>
