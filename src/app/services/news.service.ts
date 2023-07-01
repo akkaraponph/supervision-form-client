105bd098b3fe772872b5f4e8d5c28f4230099051
Akkarapon Phikulsri <akkarapon@billowdev.com>
2023-07-01 14:00:55 +0700

    Update news.service.ts

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -9,33 +9,33 @@ import { News } from '../interfaces/news';
 })
 export class NewsService {
 
-  private myAppUrl: string;
+  private API_ENDPOINT: string;
   private myApiUrl: string;
 
   constructor(private http: HttpClient) { 
 
-    this.myAppUrl = environment.endpoint;
+    this.API_ENDPOINT = environment.endpoint;
     this.myApiUrl = 'api/news/';
 
   }
 
   GetListNews(): Observable<any> {
-    return this.http.get<any>(`${this.myAppUrl}${this.myApiUrl}`);
+    return this.http.get<any>(`${this.API_ENDPOINT}${this.myApiUrl}`);
   }
 
   AddNews(news: News): Observable<void> {
-    return this.http.post<void>(`${this.myAppUrl}${this.myApiUrl}/add-news`, news);
+    return this.http.post<void>(`${this.API_ENDPOINT}${this.myApiUrl}/add-news`, news);
   }
 
   DeleteNews(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.myAppUrl}${this.myApiUrl}${id}`);
+    return this.http.delete<void>(`${this.API_ENDPOINT}${this.myApiUrl}${id}`);
   }
 
   GetNews(id: number): Observable<any> {
-    return this.http.get<any>(`${this.myAppUrl}${this.myApiUrl}${id}`)
+    return this.http.get<any>(`${this.API_ENDPOINT}${this.myApiUrl}${id}`)
   }
 
   UpdateNews(id: number, news: News): Observable<void> {
-    return this.http.put<void>(`${this.myAppUrl}${this.myApiUrl}${id}`, news);
+    return this.http.put<void>(`${this.API_ENDPOINT}${this.myApiUrl}${id}`, news);
   }
 }
